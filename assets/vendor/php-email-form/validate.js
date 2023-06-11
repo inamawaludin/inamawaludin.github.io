c0d78217e20e4d55f305888a525d1ac964f681f5
Muchammad In'am Awaludin <awaludin.sekawanmedia@gmail.com>
2023-06-11 21:47:58 +0700

    Initial Commit

---
+++ assets/vendor/php-email-form/validate.js
@@ -0,0 +1,54 @@
+/**
+* PHP Email Form Validation - v3.6
+* URL: https://bootstrapmade.com/php-email-form/
+* Author: BootstrapMade.com
+*/
+(function () {
+  "use strict";
+
+  const form = document.getElementById('form');
+  const result = document.getElementById('result');
+
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+    // result.innerHTML = "Please wait..."
+
+    let thisForm = this;
+
+    thisForm.querySelector('.loading').classList.add('d-block');
+
+      fetch('https://api.web3forms.com/submit', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json',
+                  'Accept': 'application/json'
+              },
+              body: json
+          })
+          .then(async (response) => {
+              let json = await response.json();
+              if (response.status == 200) {
+                  result.innerHTML = json.message;
+              } else {
+                  console.log(response);
+                  result.innerHTML = json.message;
+              }
+              thisForm.querySelector('.loading').classList.remove('d-block');
+          })
+          .catch(error => {
+              console.log(error);
+              thisForm.querySelector('.loading').classList.remove('d-block');
+              result.innerHTML = "Something went wrong!";
+          })
+          .then(function() {
+              form.reset();
+              setTimeout(() => {
+                  result.style.display = "none";
+              }, 3000);
+          });
+  });
+
+})();
